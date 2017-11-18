11b8676583d905cb6a2f86853994661436e4e63c
Bobby Lucas <bobbylucas622@yahoo.com>
2017-11-18 03:12:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
 var name = `Susan`;
 var height = 30
-var message = `is ${height} inches tall` {
+var message = `is ${height} inches tall` 
 ${name} is ${height} 'inches tall` 
-}
+
 
 module.exports = {name, height, message}
 console.log()
